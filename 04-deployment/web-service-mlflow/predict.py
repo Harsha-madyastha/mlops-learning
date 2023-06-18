695838b24155dbb9b3677b956e2c920c9530b3f7
Harsha-madyastha <harsha.manjunath1996@gmail.com>
2023-06-18 14:47:26 +0000

    changed from s3 gcs buckets and python version from 3.9 to 3.10

--- 04-deployment/web-service-mlflow/predict.py
+++ 04-deployment/web-service-mlflow/predict.py
@@ -7,7 +7,7 @@ from flask import Flask, request, jsonify
 
 RUN_ID = os.getenv('RUN_ID')
 
-logged_model = f's3://mlflow-models-alexey/1/{RUN_ID}/artifacts/model'
+logged_model = f'gs://mlops-data-2023/1/{RUN_ID}/artifacts/model'
 # logged_model = f'runs:/{RUN_ID}/model'
 model = mlflow.pyfunc.load_model(logged_model)
 
